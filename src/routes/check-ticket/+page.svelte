31e6afe7f028d4ad6ff680e9387811b8df9ba477
Michal Grňo <m93a.cz@gmail.com>
2023-08-25 19:58:06 +0200

    feat: přidána možnost manuálního ověření kódu

--- src/routes/check-ticket/+page.svelte
+++ src/routes/check-ticket/+page.svelte
@@ -2,6 +2,7 @@
 	import { goto } from '$app/navigation';
 	import { Scanner } from 'qr-svelte';
 	let scanning = false;
+	let manualHash = '';
 
 	const onDetected = async (data: string) => {
 		if (!scanning) return;
@@ -10,16 +11,31 @@
 		scanning = false;
 		await goto(`/check-ticket/${hash}${window.location.search}`);
 	};
+
+	const checkManually = async (hash: string) => {
+		scanning = false;
+		await goto(`/check-ticket/${hash}${window.location.search}`);
+	};
 </script>
 
-<Scanner {scanning} on:detected={({ detail: { data } }) => onDetected(data)} />
-<button on:click={() => (scanning = !scanning)}>
-  {#if scanning}
-    Zastavit skenování
-  {:else}
-    Skenovat
-  {/if}
-</button>
+<Scanner
+	{scanning}
+	maxScansPerSecond={4}
+	on:detected={({ detail: { data } }) => onDetected(data)}
+/>
+<p>
+	<button on:click={() => (scanning = !scanning)}>
+		{#if scanning}
+			Zastavit skenování
+		{:else}
+			Skenovat
+		{/if}
+	</button>
+</p>
+<p>
+	<input placeholder="Kód vstupenky" bind:value={manualHash} />
+	<button on:click={() => checkManually(manualHash)}>Ověřit manuálně</button>
+</p>
 
 <style>
 	:global(.qr > video) {
