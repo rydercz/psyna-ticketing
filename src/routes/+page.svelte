a79842395b94fd53ab1fc7f5c21bbd39ea057b73
Josef Drábek <jsf@drbk.cz>
2024-03-06 19:33:27 +0100

    dump price

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,7 +9,7 @@
 <div class="content">
 	<h1>Psyna 2024<br> předprodej vstupenek</h1>
 
-	<p>Aktuálně v prodeji druhá vlna 200 vstupenek za 850 korun českých</p>
+	<p>Aktuálně v prodeji druhá vlna 200 vstupenek<br> za 850 korun českých</p>
 
 	<p><a href="/purchase">Koupit vstupenku!</a></p>
 	<!--<p>Předprodej byl ukončen, nové lístky seženete už jen na místě.</p>-->
